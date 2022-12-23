fc3bf3aab08ca16ffa3274eb786bc69aa5286510
Lysik Aleksandr <a.lysik@bftcom.com>
2022-12-23 22:17:52 +0300

    HomeworkSecurityAcl

--- HomeworkSecurity2/src/main/java/otus/security/AclMethodSecurityConfiguration.java
+++ HomeworkSecurity2/src/main/java/otus/security/AclMethodSecurityConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AclMethodSecurityConfiguration extends GlobalMethodSecurityConfiguration {
     @SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
     @Autowired
